9001658457f840d95dc05d6337a390a809d2b779
euvasconcelos <vasconceloserick@outlook.com>
2021-07-14 12:21:41 -0300

    Incluídos cores para destacar o grau do IMC

--- CalculaIMC.py
+++ CalculaIMC.py
@@ -2,22 +2,22 @@
 def CalculaImc(peso, altura):
     imc = (peso/pow(altura,2))
     if (imc <18.5):
-        print('\nIMC: {:.2f}\n ABAIXO DO PESO'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;36mABAIXO DO PESO'.format(imc))
     
     elif (imc >18.5 and imc <=24.9):
-        print('\nIMC: {:.2f}\n PESO NORMAL'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;34mPESO NORMAL'.format(imc))
 
     elif (imc >24.9 and imc <=29.9):
-        print('\nIMC: {:.2f}\n SOBREPESO'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;33mSOBREPESO'.format(imc))
 
     elif (imc >29.9 and imc <=34.9):
-        print('\nIMC: {:.2f}\n OBESIDADE GRAU I'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;91mOBESIDADE GRAU I'.format(imc))
 
     elif (imc >34.9 and imc <=39.9):
-        print('\nIMC: {:.2f}\n OBESIDADE GRAU II'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;35mOBESIDADE GRAU II'.format(imc))
 
     else:
-        print('\nIMC: {:.2f}\n OBESIDADE GRAU III OU MÓRBIDA'.format(imc))
+        print('\nIMC: {:.2f}\n \033[1;31mOBESIDADE GRAU III OU MÓRBIDA'.format(imc))
 
 altura = float(input('ALTURA: '))
 peso = float(input('PESO: '))
